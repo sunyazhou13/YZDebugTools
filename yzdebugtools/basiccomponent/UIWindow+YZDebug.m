8ea87db22bbface7a5cecbe973fdfbec440fb845
sunyazhou <sunyazhou@autohome.com.cn>
2021-10-20 11:05:53 +0800

    [sunyazhou][update]1.更新 by shell

--- yzdebugtools/basiccomponent/UIWindow+YZDebug.m
+++ yzdebugtools/basiccomponent/UIWindow+YZDebug.m
@@ -6,45 +6,105 @@
 //
 
 #import "UIWindow+YZDebug.h"
-//#import <BlocksKit/BlocksKit.h>
+#import <BlocksKit/BlocksKit.h>
+#import <BlocksKit/NSObject+BKBlockObservation.h>
+#import "YZDebugListViewController.h"
 
 #import <objc/runtime.h>
 
-//static char *kFBMemoryProfilerKey = "FBMemoryProfilerKey";
+static char *kYZDebugVCKey = "YZDebugVCKey";
+static char *kYZShakingKey = "YZShakingKey";
+static char *kYZSActiveKey = "YZSActiveKey";
 
 @interface UIWindow ()
 
-
+@property (nonatomic, strong) YZDebugListViewController *debugVC;
+@property (nonatomic, assign) BOOL shaking;
+@property (nonatomic, assign) BOOL active;
 
 @end
 
 @implementation UIWindow (YZDebug)
 
-//- (void)setMemoryProfiler:(FBMemoryProfiler *)memoryProfiler {
-//    objc_setAssociatedObject(self, kFBMemoryProfilerKey, memoryProfiler, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-//}
-//
-//- (FBMemoryProfiler *)memoryProfiler {
-//    return objc_getAssociatedObject(self, kFBMemoryProfilerKey);
-//}
+- (void)setDebugVC:(YZDebugListViewController *)debugVC {
+    objc_setAssociatedObject(self, kYZDebugVCKey, debugVC, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (YZDebugListViewController *)debugVC {
+    return objc_getAssociatedObject(self, kYZDebugVCKey);
+}
+
+- (void)setShaking:(BOOL)shaking {
+    objc_setAssociatedObject(self, kYZShakingKey, @(shaking), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (BOOL)shaking {
+    return [objc_getAssociatedObject(self, kYZShakingKey) boolValue];
+}
+
+- (void)setActive:(BOOL)active {
+    objc_setAssociatedObject(self, kYZSActiveKey, @(active), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (BOOL)active {
+    return [objc_getAssociatedObject(self, kYZSActiveKey) boolValue];
+}
 
 - (void)enableDebug {
-    
-////    FBMemoryProfiler *memoryProfiler = [[FBMemoryProfiler alloc] init];
-//    [self bk_associateValue:@"memoryProfiler" withKey:kFBMemoryProfilerKey];
-////    FBMemoryProfiler *memoryProfilerNow = [self bk_associatedValueForKey:kFBMemoryProfilerKey];
-////    [memoryProfilerNow enable];
-//
-//    unsigned int stuIvarCount = 0;
-//    Ivar *ivars = class_copyIvarList([self class], &stuIvarCount);
-//    NSMutableArray *arr = [[NSMutableArray alloc] initWithCapacity:stuIvarCount];
-//    for (int i = 0;i<stuIvarCount;i++) {
-//        Ivar ivar = ivars[i];
-//        NSString *ivarName = [NSString stringWithUTF8String:ivar_getName(ivar)];
-//        NSLog(@"%@",ivarName);
-//        [arr addObject:ivarName];
-//    }
-//    free(ivars);
+    self bk_
+}
+
+- (void)_presentTweaks {
+     UIViewController *visibleViewController = self.rootViewController;
+     while (visibleViewController.presentedViewController != nil) {
+       visibleViewController = visibleViewController.presentedViewController;
+     }
+     
+     // Prevent double-presenting the tweaks view controller.
+     if ([visibleViewController isKindOfClass:[HYHYDebugViewController class]] ||
+         [g_navigationTopVC.presentedViewController isKindOfClass:[HYHYDebugViewController class]]) {
+         [self.debugVC dismissViewControllerAnimated:YES completion:nil];
+     } else {
+         self.debugVC = [[HYHYDebugViewController alloc] init];
+         [g_navigationTopVC presentViewController:self.debugVC animated:YES completion:nil];
+     }
+}
+
+- (BOOL)_shouldPresentTweaks{
+#ifdef DEBUG
+    return _shaking && _active;
+#else
+    return NO;
+#endif
+}
+
+- (void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+ if (motion == UIEventSubtypeMotionShake) {
+#ifdef DEBUG
+   _shaking = YES;
+   dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.4 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+     if ([self _shouldPresentTweaks]) {
+       [self _presentTweaks];
+     }
+   });
+#endif
+ }
+ [super motionBegan:motion withEvent:event];
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+ if (motion == UIEventSubtypeMotionShake) {
+   _shaking = NO;
+ }
+ [super motionEnded:motion withEvent:event];
+}
+
+- (void)motionCancelled:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (motion == UIEventSubtypeMotionShake) {
+        SYLog(@"摇动取消");
+    }
 }
 
 @end
